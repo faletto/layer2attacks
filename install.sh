448c6ebb3104f6c71d447b8fef4ae89c63d6e64e
Colin <falettocolin@gmail.com>
2025-05-06 10:23:26 -0700

    Add warning to restart terminal

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 sudo apt install ettercap-text-only yersinia -y
 sudo curl -L https://github.com/faletto/camovers/releases/download/releases/camovers-linux -o /bin/camovers
 sudo chmod +x /bin/camovers
+echo ! Installed. Please close and reopen the terminal to proceed.
