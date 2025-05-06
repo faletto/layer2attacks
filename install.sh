145abb4048c462542cefb6157349fcdcf8724842
Colin <falettocolin@gmail.com>
2025-05-06 10:25:24 -0700

    Add wireshark settings

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #/bin/bash
-sudo apt install ettercap-text-only yersinia -y
+echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
+sudo apt install ettercap-text-only yersinia wireshark -y
 sudo curl -L https://github.com/faletto/camovers/releases/download/releases/camovers-linux -o /bin/camovers
 sudo chmod +x /bin/camovers
 echo ! Installed. Please close and reopen the terminal to proceed.
