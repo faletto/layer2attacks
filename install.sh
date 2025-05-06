6bb6a26ee12704179b125daacc32c416970ae35e
Colin <falettocolin@gmail.com>
2025-05-06 10:35:28 -0700

    Add apt update

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
+sudo apt update
 sudo apt install ettercap-text-only yersinia wireshark -y
 sudo curl -L https://github.com/faletto/camovers/releases/download/releases/camovers-linux -o /bin/camovers
 sudo chmod +x /bin/camovers
