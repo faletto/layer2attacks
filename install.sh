1cf7f5c5fa80d02b88106258c22b585a58dae448
Colin <falettocolin@gmail.com>
2025-05-06 10:19:06 -0700

    Remove curl from dependencies

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-sudo apt install curl ettercap yersinia -y
+sudo apt install ettercap yersinia -y
 sudo curl -L https://github.com/faletto/camovers/releases/download/releases/camovers-linux -o /bin/camovers
 sudo chmod +x /bin/camovers
