cc933a475adf9951e1e7d911207eae17a43f9c27
Colin <falettocolin@gmail.com>
2025-05-12 12:52:57 -0700

    Add churchill, remove yersinia

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
 sudo apt update
-sudo apt install ettercap-text-only yersinia wireshark -y
+sudo apt install ettercap-text-only wireshark -y
 sudo curl -L https://github.com/faletto/camovers/releases/download/releases/camovers-linux -o /bin/camovers
 sudo chmod +x /bin/camovers
+sudo curl -L https://github.com/faletto/churchill/releases/download/v1.0.0/churchill-linux -o /bin/churchill
+sudo chmod +x /bin/churchill
 echo $'[!] Installation completed. Please note down the name of your ethernet interface shown below, you may have more than one:'
 ip -o link show | awk -F': ' '{print $2}' | grep en
 echo $'[!] If you don\'t see your interface listed, run "ip link show" to see all your interfaces.'
