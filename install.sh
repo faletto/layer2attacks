f758519ec51e0b6865ca7dfea9628b8ae90f1414
Colin <falettocolin@gmail.com>
2025-05-12 13:17:31 -0700

    Show IP along with interface

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo curl -L https://github.com/faletto/camovers/releases/download/releases/camo
 sudo chmod +x /bin/camovers
 sudo curl -L https://github.com/faletto/churchill/releases/download/v1.0.0/churchill-linux -o /bin/churchill
 sudo chmod +x /bin/churchill
-echo $'[!] Installation completed. Please note down the name of your ethernet interface shown below, you may have more than one:'
-ip -o link show | awk -F': ' '{print $2}' | grep en
+echo $'[!] Installation completed. Please note down the name/IP of your ethernet interface shown below, you may have more than one:'
+ip -brief addr | grep '^e' | awk '{print $1,$3}'
 echo $'[!] If you don\'t see your interface listed, run "ip link show" to see all your interfaces.'
 echo $'[!] Please close and reopen the terminal to proceed.'
